45de2e2677d190cd4f5e00b511c2ae93232121ff
Evgeniia_Makarova <evgetigra@gmail.com>
2022-09-01 20:09:57 +0300

    Добавлена задача 2

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int numberА = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+ if(numberА > numberB)
+            {
+                Console.Write("max = ");
+                Console.WriteLine(numberА);
+                Console.Write(" min = ");
+                Console.WriteLine(numberB);
+            }
+            else if (numberА < numberB)
+            {
+                Console.Write("max = ");
+                Console.WriteLine(numberB);
+                Console.Write("min = ");
+                Console.WriteLine(numberА);
+            }
