b55d4f8cafa512b6409268fd180eac04a4926caa
Evgeniia_Makarova <evgetigra@gmail.com>
2022-09-08 19:13:52 +0300

    добавлена задача 13

--- task_13/Program.cs
+++ task_13/Program.cs
@@ -1,8 +1,8 @@
 ﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-int Number = new Random().Next (0,9999);
+int Number = new Random().Next(0, 9999);
 Console.WriteLine(Number);
-string array =Convert.ToString(Number);
-if (array.Length>2)
+string array = Convert.ToString(Number);
+if (array.Length > 2)
 {
     Console.WriteLine(array[2] + " - третья цифра");
 }
