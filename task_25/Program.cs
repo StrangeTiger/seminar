39bff7e3ebd40b214e90613407ee2961cb66490d
Evgeniia_Makarova <evgetigra@gmail.com>
2022-09-15 19:02:36 +0300

    Добавлена задача 25

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,1 +1,23 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+using static System.Console;
+Clear();
+
+Write("Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B");
+WriteLine();
+WriteLine("Введите число А: ");
+int number = int.Parse(Console.ReadLine());
+WriteLine("Введите число B: ");
+int extent = int.Parse(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= extent; i++)
+{
+    result *= number;
+}
+
+WriteLine($"Ответ: {number} в степени {extent} = {result}");
+
+
+
+
