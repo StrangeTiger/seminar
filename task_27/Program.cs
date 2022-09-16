d73ebe6ff9ac36d5d25c289db85fa0e70c961945
Evgeniia_Makarova <evgetigra@gmail.com>
2022-09-16 17:00:54 +0300

    Добавлена задача 27

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -6,7 +6,7 @@ Clear();
 WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
 
 WriteLine("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+int number = int.Parse(ReadLine());
 int sumNumber = 0;
 
 while (number != 0)
